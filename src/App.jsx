6dfbc6b9cf22a278d3967e8b1b04e72c9247d81e
FatumaLiban2 <fatuma.omar@strathmore.edu>
2025-10-28 21:47:20 +0300

    added imports for the sign up and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Footer from './components/Footer';
 import ClientPayment from './components/ClientPayment';
 import DeveloperPayment from './components/DeveloperPayment';
 import MessagingPage from './pages/MessagingPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 import './App.css';
 
 // Layout wrapper to conditionally show Navbar/Footer
@@ -33,8 +36,10 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          {/* Placeholder routes - pages to be created later */}
-          <Route path="/" element={<div className="placeholder">Home Page</div>} />
+          {/* Main Pages */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
           <Route path="/features" element={<div className="placeholder">Features Page</div>} />
           <Route path="/about" element={<div className="placeholder">About Page</div>} />
           <Route path="/contact" element={<div className="placeholder">Contact Page</div>} />
@@ -42,8 +47,6 @@ function App() {
           <Route path="/use-cases" element={<div className="placeholder">Use Cases Page</div>} />
           <Route path="/pricing" element={<div className="placeholder">Pricing Page</div>} />
           <Route path="/blog" element={<div className="placeholder">Blog Page</div>} />
-          <Route path="/signin" element={<div className="placeholder">Sign In Page</div>} />
-          <Route path="/signup" element={<div className="placeholder">Sign Up Page</div>} />
           <Route path="/privacy" element={<div className="placeholder">Privacy Policy Page</div>} />
 
           {/* Dashboard and other related routes */}
