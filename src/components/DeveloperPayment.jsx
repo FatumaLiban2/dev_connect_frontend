7452bda26ed8334da96d1cbc7eb352372b1cc536
moha6959 <mohamedekaden576@gmail.com>
2025-11-01 13:37:32 +0300

    imported the sidebar and its stylings

--- src/components/DeveloperPayment.jsx
+++ src/components/DeveloperPayment.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Sidebar from './Sidebar'; 
+import '../styles/Sidebar.css';
 import '../styles/Payment.css';
 
 const DeveloperPayment = () => {
